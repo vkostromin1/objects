ba6897bac6942119c06fbe92894037eca552750d
Vadim Kostromin <vadim.kostromin@urfu.me>
2024-04-27 13:47:46 +0500

    Наполнение контейнеров

--- main.cpp
+++ main.cpp
@@ -19,6 +19,70 @@ DefaultHelicopter *Create_new_helicopter(HelicopterType Type)
 
 int main(){
     setlocale(LC_ALL, "Russian");
+    wstring PrintHelicopterColor (const HelicopterColor Color)
+{
+    switch(Color)
+    {
+        case HelicopterColor::Grey : return L"Серый" ;
+        case HelicopterColor::White : return L"Белый" ;
+        case HelicopterColor::Black : return L"Чёрный" ;
+        case HelicopterColor::Yellow : return L"Жёлтый" ;
+        default : return L"Неизвестно" ;
+    }
+}
+
+wstring PrintHelicopterType ( const HelicopterType Type)
+{
+    switch(Type)
+    {
+        case HelicopterType::PassHeli: return L"Пассажирский вертолёт";
+        case HelicopterType::TaxiHeli: return L"Вертолёт-таски";
+        case HelicopterType::CargoHeli: return L"Грузовой вертолёт";
+        case HelicopterType::CombatHeli: return L"Военный вертолёт";
+        default: return L"Неизвестно" ;
+    }
+}
+
+wstring PrintHelicopterSpeed (const HelicopterSpeed Speed)
+{
+    switch(Speed)
+    {
+        case HelicopterSpeed::Fast : return L"Быстрая скорость" ;
+        case HelicopterSpeed::Medium : return L"Средняя скорость" ;
+        case HelicopterSpeed::Slow : return L"Медленная скорость" ;
+        default : return L"Неизвестно" ;
+    }
+}
+
+void task1 (Iterator<HeliPtr> *Iterator) // Увеличить мощность пассажирских вертолётов
+{
+    for (Iterator->First(); !Iterator->IsDone() ; Iterator->Next())
+    {
+        int HowManyEnginePower = rand()%(40-20+1)+20;
+        const HeliPtr CurrentHeli = Iterator -> GetCurrent();
+        CurrentHeli -> AddEnginePower(HowManyEnginePower);
+        wcout<< L"Мощность вертолёта типа : " << PrintHelicopterType(CurrentHeli -> GetType()) << L" увеличилась на "  << HowManyEnginePower << endl;
+        HowManyEnginePower = 0;
+    }
+}
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    FirstContainer FirstCont ;
+    SecondContainer SecondCont ;
+
+    for (int i=0; i < rand()%100+1000; i++)
+    {
+        FirstCont.AddHeli(Create_new_helicopter(static_cast<HelicopterType>(rand()%(4)+1)));
+    }
+    wcout << L"В первом контейнере: " << FirstCont.GetCount() << L" элемента/элементов" << endl ;
+    for (int i=0; i < rand()%100+1000; i++)
+    {
+        SecondCont.AddHeli(Create_new_helicopter(static_cast<HelicopterType>(rand()%(4)+1));
+    }
+    wcout << L"Во втором контейнере: " << SecondCont.GetCount() << L" элемента/элементов" << endl;
     return 0;
+}
 }
 
